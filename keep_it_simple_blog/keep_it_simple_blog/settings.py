4b3c43cdc1e7b83d87d22b97c420b524e6025b49
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2023-09-29 20:59:45 +0100

    Debug set to false

--- keep_it_simple_blog/keep_it_simple_blog/settings.py
+++ keep_it_simple_blog/keep_it_simple_blog/settings.py
@@ -11,7 +11,7 @@ LOGIN_REDIRECT_URL = '/blog_single'
 SECRET_KEY = os.environ.get('SECRET_KEY', '123456890')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
